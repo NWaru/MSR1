23e2ee3f335046fcbf70d3d7f92da41f98d38b0b
Nosawaru Efemwonkieke <nefemwon@sfu.ca>
2023-07-18 23:19:12 -0700

    Add to bayes filter assingment

--- 2020-msr1-exercise-01-bayes-filter/assignment/ex2_1.py
+++ 2020-msr1-exercise-01-bayes-filter/assignment/ex2_1.py
@@ -24,20 +24,40 @@ def plot_belief(belief):
 
 def motion_model(action, belief):
     # add code here
+    new_belief = np.zeros(len(belief))
     if action == 'F':
         p1, p2, p3 = 0.7, 0.2, 0.1
-    elif action == 'B':
+    else:
         p1, p2, p3 = 0.1, 0.2, 0.7
 
-    belief[0] = p2*belief[0] + p3*belief[1]
-    belief[len(belief)-1] = p1*belief[len(belief)-2] + p2*belief[len(belief)-1]
+    new_belief[0] = p2*belief[0] + p3*belief[1]
+    new_belief[len(belief)-1] = p1*belief[len(belief)-2] + p2*belief[len(belief)-1]
     for i in range(1, len(belief)-1):
-        belief[i] = p1*belief[i-1] + p2*belief[i] + p3*belief[i+1]
+        new_belief[i] = p1*belief[i-1] + p2*belief[i] + p3*belief[i+1]
 
-    return belief
+    return new_belief
     
-# def sensor_model(observation, belief, world):
+def sensor_model(observation, belief, world):
     # add code here
+    p_white = 0.9
+    p_black = 0.7
 
-# def recursive_bayes_filter(actions, observations, belief, world):
+    p_hit, p_miss = (p_black, 1-p_white) if observation == 0 else (p_white, 1-p_black)
+    eta = 0
+    
+    for i in range(len(world)):
+        if world[i] == observation:
+            belief[i] *= p_hit
+        else:
+            belief[i] *= p_miss
+        eta += belief[i]
+
+    return belief / sum(belief)
+
+def recursive_bayes_filter(actions, observations, belief, world):
     # add code here
+    belief = sensor_model(observations[0], belief, world)
+    for i, action in enumerate(actions):
+            belief = motion_model(actions[i], belief)
+            belief = sensor_model(observations[i+1], belief, world)
+    return belief
